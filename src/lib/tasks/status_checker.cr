b108f0c34b067702b3f48bc6e0d224a59ddf8391
lbarasti <lbarasti@users.noreply.github.com>
2020-01-18 19:54:30 +0000

    Set connection and read timeout

--- src/lib/tasks/status_checker.cr
+++ src/lib/tasks/status_checker.cr
@@ -8,9 +8,14 @@ module StatusChecker
   record Failure, url : String, err : Exception
   private def self.get_status(url : String)
     start_time = Time.utc
-    res = HTTP::Client.get url
+    uri = URI.parse(url)
+    client = HTTP::Client.new uri
+    client.connect_timeout = 0.2.seconds
+    client.read_timeout = 1.seconds
+    res = client.get "/"
     Success.new(url, res.status_code, Time.utc - start_time)
-  rescue e : Errno | Socket::Addrinfo::Error | OpenSSL::SSL::Error
+  rescue e : Errno | Socket::Addrinfo::Error | OpenSSL::SSL::Error | IO::Timeout
+    logger.error(url + " " + e.to_s)
     Failure.new(url, e)
   end
 
